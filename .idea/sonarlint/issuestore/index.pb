81a092243adf316255f7395f1f919478391775d4
Anastasiyab92 <nastiashashok@gmail.com>
2024-10-30 13:36:54 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+B
+src/QuickSort.java,c/4/c4fe79a8afb033c9638a20fe7e9894b585c98ce4
+A
+QuickSortTask.iml,8/4/845c319df9a90731c75998b544fe906c6a3de1f1
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+Å
+Q.idea/sonarlint/securityhotspotstore/c/4/c4fe79a8afb033c9638a20fe7e9894b585c98ce4,c/6/c642699d1703afb9324b9d4bfac030319dcbf1e2
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
---
+++ src/QuickSort.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            // call recursion to sort left and right parts
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+
+        }
+    }
+    private static int partition(int[] arr, int low, int high) {
+        //choice a pivot element
+        int middle = low + (high - low) / 2;
+        int pivot = arr[middle];
+
+        //Exchange of the supporting element with the last one
+        int temp = arr[middle];
+        arr[middle] = arr[high];
+        arr[high] = temp;
+
+        int i = low-1;
+        for(int j=low; j<high; j++){
+            if(arr[j] < pivot){
+                i++;
+
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[20];
+
+        for (int i = 0; i < array.length; i++){
+            array[i] = random.nextInt(100);
+        }
+        //before quickSort
+        System.out.println(Arrays.toString(array));
+        quickSort(array, 0, array.length-1);
+        //after quickSort
+        System.out.println(Arrays.toString(array));
+    }
+
+}
